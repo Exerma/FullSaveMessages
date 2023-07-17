45a6d0d6dbb2f7917f93684a3fc727336b09f225
Patrick Seuret <patrick@seuret.ch>
2023-07-17 20:26:01 +0200

    Minimal add-on implementation

---
+++ action/action_popup.js
@@ -0,0 +1,12 @@
+/*----------------------------
+ *  (c) Patrick Seuret, 2023  
+ *----------------------------
+ *  action_popup.js
+ *----------------------------
+ *
+ * Versions:
+ *   2023-07-17: First version
+ * 
+ */
+
+ alert("Full Save Messages");
